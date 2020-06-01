4c647ead05bb0ddc3c9f9b1c25edafe4263e3d2a
Marcelo Carvalho Ferraz <marcelogfpro@hotmail.com>
2020-06-01 18:00:40 -0300

    funcionario para contato

--- agenda.js
+++ agenda.js
@@ -17,32 +17,32 @@ var app = new Vue({
 			}).
 			then(response => {
 				if (!response.data.erro)
-					this.funcionarios = response.data.funcionarios;
+					this.contatos = response.data.contatos;
 			}) 
 		},
 
 		salvarFuncionario() {
 			let form = new FormData();
-			for (let i in this.funcionario)
-				form.append(i, this.funcionario[i]);
+			for (let i in this.contato)
+				form.append(i, this.contato[i]);
 
 			axios.post(this.url, form, {
 				params: {
-					operacao: (this.funcionario.id > 0) ? "update" : "create"
+					operacao: (this.contato.id > 0) ? "update" : "create"
 				}
 			}).
 			then(response => {
 				console.log(response);
 				if (!response.data.erro) {
 					this.obterListaFuncionarios();
-					this.funcionario = {id: 0, nome: "", email: "", datanasc: "", salario: 0.00};
+					this.contato = {id: 0, nome: "", email: "", datanasc: "", salario: 0.00};
 				}
 			}); 
 
 		},
 
 		editarFuncionario(idFunc) {
-			this.funcionario = Object.assign({}, this.funcionarios.filter(f => f.id === idFunc)[0]);
+			this.contato = Object.assign({}, this.contatos.filter(f => f.id === idFunc)[0]);
 		},
 
 		removerFuncionario(idFunc) {
