2dad7d1469556d7895a7aa5b541485f21c78349d
Cassiano Medrado <cassianomoreira20@gmail.com>
2020-06-03 17:55:42 -0300

    porra

--- agenda.js
+++ agenda.js
@@ -1,15 +1,42 @@
 var app = new Vue({
     el: "#app",
     data: {
-        contato: {id: 0, nome: "", email: "", telefone: "", intagram: "", facebook:""},
+        default: {} = { id: null, nome: "", email: null, telefone: "", instagram: null, facebook: null },
+        contato: {} = { id: null, nome: "", email: null, telefone: "", instagram: null, facebook: null },
         contatos: [] = [],
         url: "./agenda.php"
     },
     created: function() {
         this.ListarContatos();
     },
-	methods: 
+    methods:         
 	{
+        CheckFields() {
+            // i = 0 = id
+            // i = 1 = nome
+            // i = 2 = email
+            // i = 3 = telefone
+            // i = 4 = instagram
+            // i = 5 = facebook
+            
+            var continua = true;
+
+            console.log(this.contato[1]);
+            console.log(this.contato[3]);
+            if (typeof (this.contato[1]) == "undefined") {
+                alert("Nome não informado!");
+                continua = false;
+                this.contato = this.default;
+            }
+            if (typeof (this.contato[3]) == "undefined") {
+                alert("Número de Telefone não informado!");
+                continua = false;
+                this.contato = this.default;
+            }
+            
+            return continua;
+        },
+
 		ListarContatos() {
 			axios.get(this.url, {
 				params: {
@@ -17,30 +44,37 @@ var app = new Vue({
 				}
 			}).then(response => {
 				if (!response.data.erro){
-					console.log(response.data); // undefined assistir aula 1205 e 1805
-					this.contatos = response.data.contatos;					
-					console.log(this.contatos); // undefined
+                    // console.log(response.data.contatos);
+                    this.contatos = response.data.contatos;
+					// console.log(this.contatos); // undefined
 				}
 			}) 
 		},
 
         salvarContato() {
-            let form = new FormData();
-            for (let i in this.contato)
-                form.append(i, this.contato[i]);
+            // if (this.CheckFields()) {
+                let form = new FormData();
+                for (let i in this.contato)
+                    form.append(i, this.contato[i]);
+    
+                    
+                axios.post(this.url, form, {
+                    params: {
+                        operacao: (this.contato.id > 0) ? "update" : "create"
+                    }
+                }).
+                then(response => {
+                    alert(response.data.msg);
+                    
+                    if (!response.data.erro) {
+                        this.ListarContatos();
+                        this.contato = this.default;
+                    } else {
+                        // location.reload();
+                    }
+                });
+            // }
 
-            axios.post(this.url, form, {
-                params: {
-                    operacao: (this.contato.id > 0) ? "update" : "create"
-                }
-            }).
-            then(response => {
-                console.log(response);
-                if (!response.data.erro) {
-                    this.ListarContatos();
-                    this.contato = {id: 0, nome: "", email: "", telefone: "", intagram: "", facebook:""};
-                }
-            });
         },
 
         editarContato(idContato) {
@@ -55,8 +89,13 @@ var app = new Vue({
                 }
             }).
             then(response => {
-                if (!response.data.erro)
+                alert(response.data.msg);
+
+                if (!response.data.erro){
                     this.ListarContatos();
+                } else {
+                    // location.reload();
+                }
             })             
         }
     }
--- agenda.php
+++ agenda.php
@@ -14,13 +14,12 @@
     $resultado = array("erro" => false); // VETOR PARA GUARDAR ERROS
 
     $operacao = "";
-    if (isset($_GET["operacao"]))
+    if (isset($_GET["operacao"])){
         $operacao = $_GET["operacao"];
-
-    echo $operacao;
-
+    }
+    
     if ($operacao == "read") { // LISTA OS CONTATOS PELA LEITURA 
-        $sql = "select * from contatos";
+        $sql = "select * from contato";
         if (isset($_GET["id"])) // SE O ID JÁ FOR PASSADO, CONDIÇÃO PARA PROCURAR ESTE ID
             $sql .= " where id = $_GET[id]";
         $exec = $conexao->query($sql); 
@@ -41,45 +40,56 @@
         $instagram = $_POST["instagram"];
         $facebook = $_POST["facebook"];
 
-        if (empty($nome)) {
+        if (empty($nome) || empty($telefone)) {
             $resultado["erro"] = true;
-            $resultado["msg"] = "Nome nao informado!";
+            $resultado["msg"] = "Verifique os campos obrigatórios";
         } else {            
-            $sql = "Insert into contatos (nome, email, telefone, instagram, facebook) 
-                    values ('$nome', '$email', '$telefone', '$instagram', '$facebook')";
+            $sql = "Insert into contato (nome, email, telefone, instagram, facebook) values ('$nome', '$email', '$telefone', '$instagram', '$facebook')";
             if (!$conexao->query($sql)) {
                 $resultado["erro"] = true;
                 $resultado["msg"] = $conexao->error;
+            } else {
+                $resultado["erro"] = false;
+                $resultado["msg"] = "Novo contato inserido com sucesso";
             }
         }
 
     } else
     if ($operacao == "update") { // EDITAR CONTATO
+        $id = $_POST["id"];
         $nome = $_POST["nome"];
         $email = $_POST["email"];
         $telefone = $_POST["telefone"];
         $instagram = $_POST["instagram"];
         $facebook = $_POST["facebook"];
-        $linkedin = $_POST["linkedin"];
 
-        $sql = "Update contatos set nome = '$nome', email = '$email', '$telefone', 
-                '$instagram', '$facebook','$linkedin' where id =$id";
-
-        if (!$conexao->query($sql)) { // TESTA SE HOUVE ERRO NO COMANDO SQL
+        if (empty($nome) || empty($telefone)) {
             $resultado["erro"] = true;
-            $resultado["msg"] = $conexao->error;
+            $resultado["msg"] = "Verifique os campos obrigatórios";
+        } else { 
+            $sql = "Update contato set nome = '$nome', email = '$email', telefone = '$telefone', instagram = '$instagram', facebook = '$facebook' where id =$id";
+
+            if (!$conexao->query($sql)) { // TESTA SE HOUVE ERRO NO COMANDO SQL
+                $resultado["erro"] = true;
+                $resultado["msg"] = $conexao->error;
+            } else {
+                $resultado["erro"] = false;
+                $resultado["msg"] = "Contato atualizado com sucesso!";
+            }
         }
     } else
     if ($operacao == "delete") { // DELETAR CONTATO
 
         $id = $_GET["id"];
         
-        $sql = "delete from contatos where id = $id"; // DELETAR CONTATO PELO ID ESCOLHIDO
+        $sql = "delete from contato where id = $id"; // DELETAR CONTATO PELO ID ESCOLHIDO
         if (!$conexao->query($sql)) {
             $resultado["erro"] = true;
             $resultado["msg"] = $conexao->error;
-        } else
+        } else{
+            $resultado["erro"] = false;
             $resultado["msg"] = "Contato removido com sucesso!";
+        }
     }
 
     echo json_encode($resultado);
