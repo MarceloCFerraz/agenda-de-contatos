86dc13e0145c85f4531f7c672ceff1f1fa938a06
Cassiano Medrado <cassianomoreira20@gmail.com>
2020-06-03 08:33:42 -0300

    minha rola

--- agenda.js
+++ agenda.js
@@ -1,7 +1,7 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-        contato: {id: 0, nome: "", email: "", telefone: "", intagram: "", facebook:"",linkedin:""},
+		contato:{id:0, nome: "", email: "", telefone: ""},
 		contatos: [],
 		url: "./agenda.php"
 	},
@@ -15,47 +15,10 @@ var app = new Vue({
 					operacao: "read"
 				}
 			}).
-			then(response => {
-				if (!response.data.erro)
-					this.contatos = response.data.contatos;
-			}) 
-		},
-
-		salvarContato() {
-			let form = new FormData();
-			for (let i in this.contato)
-				form.append(i, this.contato[i]);
-
-			axios.post(this.url, form, {
-				params: {
-					operacao: (this.contato.id > 0) ? "update" : "create"
-				}
-			}).
-			then(response => {
-				console.log(response);
-				if (!response.data.erro) {
-					this.ListarContatos();
-					this.contato = {id: 0, nome: "", email: "", telefone: "", intagram: "", facebook:"",linkedin:""};
-				}
-			}); 
-
-		},
-
-		editarContato(idContato) {
-			this.contato = Object.assign({}, this.contatos.filter(c => c.id === idContato)[0]);
-		},
-
-		removerContato(idContato) {
-			axios.get(this.url, {
-				params: {
-					operacao: "delete",
-					id: idContato
-				}
-			}).
-			then(response => {
-				if (!response.data.erro)
-					this.ListarContatos();
-			}) 			
+			then(response =>{
+				console.log(response.data);
+			})
 		}
+
 	}
 })
--- agenda.php
+++ agenda.php
@@ -4,82 +4,35 @@
 	$senha = "";
 	$db = "agenda";
 
+	/*
 	$conexao = new mysqli($host, $usuario, $senha, $db);
 
-	if ($conexao->connect_error) { // VERFICA SE A CONEXÃO TEVE ERROS
+	if($conexao->connect_error){
 		echo $conexao->connect_error;
 		exit;
 	}
+	*/
 
-	$resultado = array("erro" => false); // VETOR PARA GUARDAR ERROS
+	$result = array("error" => false, "contatos" => []);
 
 	$operacao = "";
 	if (isset($_GET["operacao"]))
 		$operacao = $_GET["operacao"];
 
-	if ($operacao == "read") { // LISTA OS CONTATOS PELA LEITURA 
-		$sql = "Select * From contatos";
-		if (isset($_GET["id"])) // SE O ID JÁ FOR PASSADO, CONDIÇÃO PARA PROCURAR ESTE ID
-			$sql .= " where id = $_GET[id]";
-		$exec = $conexao->query($sql); 
-
-		$contatos = array();
-		while ($contatos = $exec->fetch_assoc())
-			array_push($contatos, $contatos);
-
-		$resultado["registros"] = $exec->num_rows;
-		$resultado["contatos"] = $contatos;
-
-	} else
-	if ($operacao == "create") { // INCLUIR CONTATO
-		$nome = $_POST["nome"];
-		$email = $_POST["email"];
-		$telefone = $_POST["telefone"];
-		$instagram = $_POST["instagram"];
-		$facebook = $_POST["facebook"];
-		$linkedin = $_POST["linkedin"];
-
-		if (empty($nome)) {
-			$resultado["erro"] = true;
-			$resultado["msg"] = "Nome nao informado!";
-		} else {			
-			$sql = "Insert into contatos (nome, email, telefone, instagram, facebook, likedin) 
-					values ('$nome', '$email', '$telefone', '$instagram', '$facebook','$linkedin')";
-			if (!$conexao->query($sql)) {
-				$resultado["erro"] = true;
-				$resultado["msg"] = $conexao->error;
-			}
-		}
-
-	} else
-	if ($operacao == "update") { // EDITAR CONTATO
-		$nome = $_POST["nome"];
-		$email = $_POST["email"];
-		$telefone = $_POST["telefone"];
-		$instagram = $_POST["instagram"];
-		$facebook = $_POST["facebook"];
-		$linkedin = $_POST["linkedin"];
-
-		$sql = "Update contatos set nome = '$nome', email = '$email', '$telefone', 
-				'$instagram', '$facebook','$linkedin' where id =$id";
-
-		if (!$conexao->query($sql)) { // TESTA SE HOUVE ERRO NO COMANDO SQL
-			$resultado["erro"] = true;
-			$resultado["msg"] = $conexao->error;
-		}
-	} else
-	if ($operacao == "delete") { // DELETAR CONTATO
-
-		$id = $_GET["id"];
+	/*
+	if ($operacao == "read") {
+		$sql = "select * from contatos";
+		$exec = $conexao->query($sql);
 		
-		$sql = "delete from contatos where id = $id"; // DELETAR CONTATO PELO ID ESCOLHIDO
-		if (!$conexao->query($sql)) {
-			$resultado["erro"] = true;
-			$resultado["msg"] = $conexao->error;
-		} else
-			$resultado["msg"] = "Contato removido com sucesso!";
-	}
+		$contatos = array();
+		while($contato = $exec->fetch_assoc())
+			array_push($contatos, $contato);
 
-	echo json_encode($resultado);
+			$result ["registros"] = $exec->num_rows;
+			$result["contatos"] = $contatos;
+	}
+	*/
+
+	echo json_encode($result);
 
 ?>
